55c0c8076618313f1d53088bb09d032e380d9207
jeremytzh <jeremytanstone@gmail.com>
2026-01-09 00:10:10 +0800

    clean up code blocks

--- ros2plot/effects/graph_plot.py
+++ ros2plot/effects/graph_plot.py
@@ -217,29 +217,6 @@ class Plot(GraphEffect):
     
     def do_plot(self):
         grid = self._grid
-        # for i in range(self._cfg.width):
-        #     for j in range(self._cfg.height):
-        #         if self._plt.high_def:
-        #             b_x = i*2
-        #             b_y = j*4
-        #             braille_cells = []
-
-        #             dot_offsets = [
-        #                 (0, 0), (0, 1), (0, 2),
-        #                 (1, 0), (1, 1), (1, 2),
-        #                 (0, 3), (1, 3),
-        #             ]
-
-        #             for dot_num, (dx, dy) in enumerate(dot_offsets, start=1):
-        #                 if grid.at(grid.to_index(b_x + dx, b_y + dy)):
-        #                     braille_cells.append(dot_num)
-
-        #             if len(braille_cells) > 0:
-        #                 self.e_print(braille_char(braille_cells), i, j, self._plt.colour)
-        #         else:
-        #             if grid.at(grid.to_index(i, j)):
-        #                 self.e_print("*", i, j, self._plt.colour)
-
         # self.debug_print(f"plotting {len(changed_grid_cells)} grid cells")
         done_ledger = set() # set to store done cells so we ignore overlaps
         for i,j in self._plot_cell_buffer:
@@ -279,64 +256,3 @@ class Plot(GraphEffect):
         last_y_index = get_mapped_value(self._scanlines[-1].last, self._cfg.y_max_value, 0, self._cfg.y_min_value, self._cfg.height)
         if last_y_index < self._cfg.height+1 and last_y_index >= 0:
             self.e_print(f"{self._scanlines[-1].last:3.2f}", last_x_index+1, last_y_index, self._plt.colour)
-
-'''
-    def do_plot(self, y_data, x_data):
-        n_vals = len(x_data)
-        use_braille = self._plt.high_def
-
-        width = self._cfg.width*2 if use_braille else self._cfg.width
-        height = self._cfg.height*4 if use_braille else self._cfg.height
-
-        grid = Grid(width, height)
-        prior_x = -1
-        prior_y = -1
-        last = None
-        for x_val, y_val in zip(x_data, y_data):
-            y_index = get_mapped_value(y_val, self._cfg.y_max_value, 0, self._cfg.y_min_value, height-1) # flipped min and max because asciimatics y=0 is the topmost row of terminal.
-            x_index = get_mapped_value(x_val, self._cfg.x_max_value, width-1, self._cfg.x_min_value, 0)
-
-            if x_index > width-1 or x_index < 0 or y_index > height-1 or y_index < 0:
-                continue
-
-            if self._plt.interpolate and last != None:
-                for pt in bresenham(x_index, y_index, prior_x, prior_y):
-                    if pt[0] > width-1 or pt[0] < 0 or pt[1] > height-1 or pt[1] < 0:
-                        continue
-                    grid.set_value(grid.to_index(pt[0], pt[1]), True)
-            else:
-                grid.set_value(grid.to_index(x_index, y_index), True)
-            prior_x = x_index
-            prior_y = y_index
-            last = (x_val, y_val)
-        
-        # parse chars
-        for i in range(self._cfg.width):
-            for j in range(self._cfg.height):
-                if use_braille:
-                    b_x = i*2
-                    b_y = j*4
-                    braille_cells = []
-
-                    dot_offsets = [
-                        (0, 0), (0, 1), (0, 2),
-                        (1, 0), (1, 1), (1, 2),
-                        (0, 3), (1, 3),
-                    ]
-
-                    for dot_num, (dx, dy) in enumerate(dot_offsets, start=1):
-                        if grid.at(grid.to_index(b_x + dx, b_y + dy)):
-                            braille_cells.append(dot_num)
-
-                    if len(braille_cells) > 0:
-                        self.e_print(braille_char(braille_cells), i, j, self._plt.colour)
-                else:
-                    if grid.at(grid.to_index(i, j)):
-                        self.e_print("*", i, j, self._plt.colour)
-
-        # print last value
-        if last != -1:
-            x_latest_location = min(get_mapped_value(x_data[last], self._cfg.x_max_value, self._cfg.width-1, self._cfg.x_min_value, 0), self._cfg.width)
-            y_latest_location = get_mapped_value(y_data[last], self._cfg.y_max_value, 0, self._cfg.y_min_value, self._cfg.height)
-            self.e_print(f"{y_data[last]:3.2f}", x_latest_location+1, y_latest_location, self._plt.colour)
-'''
