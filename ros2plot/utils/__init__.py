89a50210a7e05b8e68fe5f96d0c3c5c39e3ae03d
jeremytzh <jeremytanstone@gmail.com>
2026-02-19 10:44:05 +0800

    split plotdatahandler and graph data

--- ros2plot/utils/__init__.py
+++ ros2plot/utils/__init__.py
@@ -1,7 +1,8 @@
 
 from .colour_palette import COLOURS_LIST, NUM_COLOURS
 from .graph_math import min_max, get_mapped_value, bresenham
-from .graph_data import GraphConfigs, PlotData, RosPlotDataHandler, TIMESTAMP_KEY
+from .graph_data import GraphConfigs, PlotData
+from .plot_data_handler import RosPlotDataHandler, TIMESTAMP_KEY
 from .arguments import get_args, TOPIC_NAME, TOPIC_TYPE, FIELDS, X_FIELD, CSV, CSV_DEFAULT_X_KEY, LOG_STATS
 from .braille import braille_char
 from .grid import Grid
